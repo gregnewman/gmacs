09bbddae1e1022d3b9d3ba8a6683d827183c893d
Greg Newman <greg@gregnewman.org>
2018-10-24 11:04:29 -0400

    initial config and init

---
+++ init.el
@@ -0,0 +1,18 @@
+;; The main entry point into my emacs config.
+;; It either loads the pre-compiled GMACS configuration file,
+;; or tangles and loades the GMACS literate org configuration file.
+
+(advice-add #'display-startup-echo-area-message :override #'ignore)
+(setq inhibit-startup-message t
+      inhibit-startup-echo-area-message user-login-name
+      inhibit-default-init t
+      initial-major-mode 'fundamental-mode
+      initial-scratch-message nil
+      mode-line-format nil)
+
+
+(let ((file-name-handler-alist nil))
+  (if (file-exists-p (expand-file-name "gmacs.elc" user-emacs-directory))
+      (load-file (expand-file-name "gmacs.elc" user-emacs-directory))
+    (require 'org)
+    (org-babel-load-file (expand-file-name "gmacs.org" user-emacs-directory))))
