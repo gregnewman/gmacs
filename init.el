947c170a7850a3a15404c8d43c7ff82607161c3a
Greg Newman <greg@gregnewman.org>
2023-05-23 19:19:48 -0400

    :hammer: cleaning up a bunch of :poop:

--- init.el
+++ init.el
@@ -134,22 +134,4 @@ Note the weekly scope of the command's precision.")
 	  (my-tangle-gmacs.org))))
 (add-hook 'after-save-hook 'my-tangle-gmacs.org-hook-func)
 
-
 (message "→★ loading init.el in %.2fs" (float-time (time-subtract (current-time) my-init-el-start-time)))
-(custom-set-variables
- ;; custom-set-variables was added by Custom.
- ;; If you edit it by hand, you could mess it up, so be careful.
- ;; Your init file should contain only one such instance.
- ;; If there is more than one, they won't work right.
- '(custom-safe-themes
-   '("eca44f32ae038d7a50ce9c00693b8986f4ab625d5f2b4485e20f22c47f2634ae" "1a1ac598737d0fcdc4dfab3af3d6f46ab2d5048b8e72bc22f50271fd6d393a00" default)))
-(custom-set-faces
- ;; custom-set-faces was added by Custom.
- ;; If you edit it by hand, you could mess it up, so be careful.
- ;; Your init file should contain only one such instance.
- ;; If there is more than one, they won't work right.
- '(fixed-pitch ((t (:family "Fira Code" :height 120))))
- '(org-block ((t (:inherit fixed-pitch))))
- '(org-pomodoro-mode-line ((t (:foreground "#ff5555"))))
- '(org-pomodoro-mode-line-break ((t (:foreground "#50fa7b"))))
- '(org-table ((t (:inherit fixed-pitch)))))
