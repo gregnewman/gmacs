2fbe9e12a6f5be1d99c6b2850fac30a86cd1af1f
Greg Newman <greg@gregnewman.org>
2025-07-02 15:26:50 -0400

    Updating early init

--- early-init.el
+++ early-init.el
@@ -1,5 +1,14 @@
 ;; HACK Work around native compilation on macOS failing with 'ld: library not
 ;; found for -lemutls_w'.
 ;; https://github.com/d12frosted/homebrew-emacs-plus/issues/554 and https://github.com/d12frosted/homebrew-emacs-plus/issues/323
-
 (setenv "LIBRARY_PATH" "/opt/homebrew/opt/gcc/lib/gcc/14:/opt/homebrew/opt/libgccjit/lib/gcc/current:/opt/homebrew/opt/gcc/lib/gcc/14/gcc/aarch64-apple-darwin24/14")
+
+(setq frame-resize-pixelwise t
+      frame-inhibit-implied-resize 'force
+      frame-title-format '("%b")
+      ring-bell-function 'ignore
+      use-dialog-box t ; only for mouse events, which I seldom use
+      use-file-dialog nil
+      use-short-answers t
+      inhibit-splash-screen t
+      inhibit-startup-screen t)
